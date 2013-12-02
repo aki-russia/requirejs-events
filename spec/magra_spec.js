b3248c564e35ff92f7a0d82fc47acc6afa448a60
Dmitriy Kharchenko <akimoto@yandex.ru>
2013-12-02 18:42:03 +0700

    started with specs

---
+++ spec/magra_spec.js
@@ -0,0 +1,9 @@
+describe('mangra events spec', function(){
+
+  describe('creating events', function(){});
+  describe('initialize objects with events bus', function(){});
+  describe('binding / unbinding handlers', function(){});
+  describe('firing events', function(){});
+  describe('events bus, sprouting new', function(){});
+
+});
